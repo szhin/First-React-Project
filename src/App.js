89c9e57afbf181bccfff8284572664bbcb0ff464
bangpham <uzumakinarutoshin@gmail.com>
2023-02-25 11:19:21 +0700

    Button and HeroSection

--- src/App.js
+++ src/App.js
@@ -1,11 +1,19 @@
-import "./App.css";
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Navbar from './components/Navbar/Navbar';
+
+import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <h1>Yo</h1>
-    </div>
-  );
+    return (
+        <>
+            <Router>
+                <Navbar />
+                <Routes>
+                    <Route path="/" exact />
+                </Routes>
+            </Router>
+        </>
+    );
 }
 
 export default App;
