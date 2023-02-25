55e90dd17af22bb94e6277ea187abd699b74ef2c
bangpham <uzumakinarutoshin@gmail.com>
2023-02-25 11:55:31 +0700

    HeroSection

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Navbar from './components/Navbar/Navbar';
 
 import './App.css';
+import Home from './components/pages/Home';
 
 function App() {
     return (
         <>
             <Router>
                 <Navbar />
+                <Home />
                 <Routes>
                     <Route path="/" exact />
                 </Routes>
--- src/components/HeroSection/HeroSection.js
+++ src/components/HeroSection/HeroSection.js
@@ -1,4 +1,4 @@
-import { Button } from './Button';
+import { Button } from '../Button/Button';
 import './HeroSection.css';
 import '../../App.css';
 
