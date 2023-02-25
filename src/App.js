e339d4c5f7aaedfcbf06b9abe5d25ea4125f3547
bangpham <uzumakinarutoshin@gmail.com>
2023-02-25 13:03:54 +0700

    Create Cards component && Router application

--- src/App.js
+++ src/App.js
@@ -1,17 +1,39 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import {
+    BrowserRouter as Router,
+    Routes,
+    Route,
+} from 'react-router-dom';
 import Navbar from './components/Navbar/Navbar';
 
 import './App.css';
 import Home from './components/pages/Home';
+import Services from './components/pages/Services';
+import Products from './components/pages/Products';
+import Signup from './components/pages/Signup';
 
 function App() {
     return (
         <>
             <Router>
                 <Navbar />
-                <Home />
                 <Routes>
-                    <Route path="/" exact />
+                    <Route
+                        path="/"
+                        exact
+                        element={<Home />}
+                    />
+                    <Route
+                        path="/services"
+                        element={<Services />}
+                    />
+                    <Route
+                        path="/products"
+                        element={<Products />}
+                    />
+                    <Route
+                        path="/sign-up"
+                        element={<Signup />}
+                    />
                 </Routes>
             </Router>
         </>
--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -1,10 +1,12 @@
 import '../../App.css';
+import Cards from '../Cards/Cards';
 import HeroSection from '../HeroSection/HeroSection';
 
 function Home() {
     return (
         <>
             <HeroSection />
+            <Cards />
         </>
     );
 }
